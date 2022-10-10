3349bc81b235fc46eff2c6c55f5c52d56a6354b8
tannazma <tannaz.mohamadi.akbari@gmail.com>
2022-10-10 22:49:55 +0400

    remove updates from HTML and add in js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function selectMenu(menuItem) {
     menuItem.classList.add("Selected");
 }
+
+const updates = document.createElement("div");
+updates.textContent = " Updates";
+updates.onclick = function () {
+    selectMenu(updates)
+}
+
+const eight = document.createElement("span");
+eight.textContent = 8;
+
+const menu = document.querySelector(".Menu");
+
+updates.prepend(eight);
+menu.appendChild(updates)
