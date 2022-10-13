a7abb864eb19bd2d0e56de5c3fbdaab444a7aa75
tannazma <tannaz.mohamadi.akbari@gmail.com>
2022-10-13 18:50:06 +0400

    script for develop and categories

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const menu = document.querySelector(".Menu");
+
 function selectMenu(menuItem) {
     menuItem.classList.add("Selected");
 }
+
 
 const updates = document.createElement("div");
 updates.textContent = " Updates";
@@ -11,7 +14,28 @@ updates.onclick = function () {
 const eight = document.createElement("span");
 eight.textContent = 8;
 
-const menu = document.querySelector(".Menu");
-
 updates.prepend(eight);
 menu.appendChild(updates)
+
+const categories = document.createElement("div");
+categories.textContent = " Categories";
+categories.onclick = function () {
+    selectMenu(categories)
+}
+
+const seven = document.createElement("span");
+seven.textContent = 7;
+
+categories.prepend(seven);
+menu.appendChild(categories);
+
+const develop = document.createElement("div");
+develop.textContent = " Develop";
+develop.onclick = function () {
+    selectMenu(develop)
+}
+const six = document.createElement("span");
+six.textContent = 6;
+
+develop.prepend(six);
+menu.appendChild(develop);
