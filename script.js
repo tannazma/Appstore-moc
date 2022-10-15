93998980f08e059db4e5d761bd9012c018b02eae
tannazma <tannaz.mohamadi.akbari@gmail.com>
2022-10-16 00:17:21 +0400

    add applist by javascript and remove from html

--- script.js
+++ script.js
@@ -33,3 +33,36 @@ menuItems.forEach(function (menuItem) {
     menu.appendChild(menuItemDiv);
 })
 
+const applist = [
+    { id: 1, title: "LiquidText", description: "Annotate and review documents" }, { id: 2, title: "BBEdit", description: "Legendary text and code editor" },{ id: 2, title: "BBEdit", description: "Legendary text and code editor" }];
+
+const appListContainer = document.querySelector("#app-list");
+applist.forEach(function (itemApp) {
+    const itemAppDiv = document.createElement("div");
+    itemAppDiv.className = "app-item";
+
+    const getButton = document.createElement("button");
+    getButton.textContent = "Get";
+
+    const firstDivInsideEachAppDiv = document.createElement("div");
+    firstDivInsideEachAppDiv.textContent = itemApp.title;
+
+
+    const appDescription = document.createElement("p");
+    appDescription.textContent = itemApp.description;
+
+
+    const itemAppIdSpan = document.createElement("span");
+    itemAppIdSpan.textContent = itemApp.id + " ";
+
+    itemAppDiv.append(firstDivInsideEachAppDiv);
+    firstDivInsideEachAppDiv.append(appDescription)
+
+
+    appListContainer.append(itemAppDiv);
+
+    const getButtonDiv = document.createElement("div");
+    getButtonDiv.appendChild(getButton);
+    itemAppDiv.appendChild(getButtonDiv);
+    firstDivInsideEachAppDiv.prepend(itemAppIdSpan);
+})
